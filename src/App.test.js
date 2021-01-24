a0118ee2349cf3682a6b34b47a87016795b2c2b3
Tomas Cornelles <tomascornelles@gmail.com>
2021-01-24 11:47:41 +0100

    init test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const mainTitle = screen.getByText(/animal lovers/i);
+  expect(mainTitle).toBeInTheDocument();
 });
